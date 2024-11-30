bfe65e3b8e08f856105d117a67b62ff5d80ac587
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-11-30 19:51:40 +0530

    adding cors stuff in config

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,53 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false,
-    experimental: {
-      serverActions: {
-        bodySizeLimit: "4mb",
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
+    ];
+  },
+  reactStrictMode: false,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
     },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-          port: "",
-        },
-        {
-          protocol: "https",
-          hostname: "utfs.io",
-          port: "",
-          pathname: "/f/**",
-        },
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-          port: "",
-        },
-      ],
-    },
-  };
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/f/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
